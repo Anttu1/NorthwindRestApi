5023452069dceadf7b1f4c4641670f2e37e639d3
Anna-Maria Sairanen <anna-maria.sairanen@student.careeria.fi>
2024-09-20 15:21:06 +0300

    Lisäystoiminto

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NorthwindRestApi.Models;
+using System.Linq.Expressions;
 
 namespace NorthwindRestApi.Controllers
 {
@@ -15,8 +16,53 @@ namespace NorthwindRestApi.Controllers
         [HttpGet]
         public ActionResult GetAllCustomers() 
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe, lue lisää:" + ex);
+            }
+        }
+
+        //Hakee yhden asiakkaan pääavaimella
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    return BadRequest("Asiakasta ei löytynyt tällä id:llä:" + id);
+                    //return BadRequest($"Asiakasta ei löytnyt tällä id:llä: {id}");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest( "Tapahtui virhe, lue lisää:" + ex);
+            }
+        }
+        //Uuden lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Customer cust)
+        {
+            try
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok($"Lisättiin uusi asiakas: {cust.CompanyName} from {cust.City}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe, lue lisää:" + ex.InnerException);
+            }
         }
     }
 }

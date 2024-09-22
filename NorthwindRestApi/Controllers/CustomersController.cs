17f6ac5510eda1032c00aeb8a558339b21c2c759
Anna-Maria Sairanen <anna-maria.sairanen@student.careeria.fi>
2024-09-22 19:51:47 +0300

    Lisätty cors-määritys

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -8,22 +8,34 @@ namespace NorthwindRestApi.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class CustomersController : ControllerBase
-    {   
+    {
         //Tietokantayhteyden alustaminen
-        NorthwindOriginalContext db = new NorthwindOriginalContext();
+
+        //Perinteinen tapa
+        //(private) NorthwindOriginalContext db = new NorthwindOriginalContext();
+
+        //Dependency Injektion tapa
+        private NorthwindOriginalContext db;
+
+        public CustomersController(NorthwindOriginalContext dbparametri)
+        {
+            db = dbparametri;
+        }
+
+
 
         //Hakee kaikki asiakkaat
         [HttpGet]
-        public ActionResult GetAllCustomers() 
+        public ActionResult GetAllCustomers()
         {
             try
             {
                 var asiakkaat = db.Customers.ToList();
                 return Ok(asiakkaat);
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                return BadRequest("Tapahtui virhe, lue lisää:" + ex);
+                return BadRequest("Tapahtui virhe, lue lisää:" + e);
             }
         }
 
@@ -44,9 +56,9 @@ namespace NorthwindRestApi.Controllers
                     //return BadRequest($"Asiakasta ei löytnyt tällä id:llä: {id}");
                 }
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                return BadRequest( "Tapahtui virhe, lue lisää:" + ex);
+                return BadRequest("Tapahtui virhe, lue lisää:" + e);
             }
         }
         //Uuden lisääminen
@@ -59,9 +71,75 @@ namespace NorthwindRestApi.Controllers
                 db.SaveChanges();
                 return Ok($"Lisättiin uusi asiakas: {cust.CompanyName} from {cust.City}");
             }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe, lue lisää:" + e.InnerException);
+            }
+        }
+        //Asiakkaan poistaminen
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+
+                if (asiakas != null)
+                {
+                    db.Customers.Remove(asiakas);
+                    db.SaveChanges();
+                    return Ok("Asiakas" + asiakas.CompanyName + "poistettiin.");
+                }
+                return NotFound("Asiakasta id:llä" + id + "ei löytynyt.");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+        //Muokkaa
+        [HttpPut("{id}")]
+        public ActionResult EditCustomer(string id, [FromBody] Customer customer)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+                if (asiakas != null)
+                {
+                    asiakas.CompanyName = customer.CompanyName;
+                    asiakas.ContactName = customer.ContactName;
+                    asiakas.Address = customer.Address;
+                    asiakas.City = customer.City;
+                    asiakas.Region = customer.Region;
+                    asiakas.PostalCode = customer.PostalCode;
+                    asiakas.Country = customer.Country;
+                    asiakas.Phone = customer.Phone;
+                    asiakas.Fax = customer.Fax;
+
+                    db.SaveChanges();
+                    return Ok("Muokattu asiakasta: " + asiakas.CompanyName);
+                }
+                return NotFound("Asiakasta ei löytynyt id:llä: " + id);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+        //Hakee nimen osalla: /api/companyname/hakusana
+        [HttpGet("companyname/{cname}")]
+        public ActionResult GetByName(string cname)
+        {
+            try
+            {
+                var cust = db.Customers.Where(c => c.CompanyName.Contains(cname));
+                //var cust = from c in db.Customers where c.CompanyName.Contains(cname) select c; <--- sama eri muodossa
+                //var cust = db.Customers.Where(c => c.CompanyName == cname); <--- perfect match
+                return Ok(cust);
+            }
             catch (Exception ex)
             {
-                return BadRequest("Tapahtui virhe, lue lisää:" + ex.InnerException);
+                return BadRequest(ex.Message);
             }
         }
     }
--- NorthwindRestApi/Models/NorthwindOriginalContext.cs
+++ NorthwindRestApi/Models/NorthwindOriginalContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace NorthwindRestApi.Models;
 
@@ -76,9 +78,11 @@ public partial class NorthwindOriginalContext : DbContext
     public virtual DbSet<TuoteSummat> TuoteSummats { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=ANA\\SQLEXANNAMARIASA;Database=NorthwindOriginal;Trusted_Connection=True;;TrustServerCertificate=true");
-
+    {
+        if (!optionsBuilder.IsConfigured) {
+            return;
+        }
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
